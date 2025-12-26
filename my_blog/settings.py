e4ee1c8515596e11fa77696fb53b93fae782e9b5
Sonu Kumar <sonukumaryz463@gmail.com>
2025-12-26 20:49:52 +0530

    Update settings.py

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-py%ho3t0_#tfu(40mvv2nf0p&zua!-zgy#)_4ay94)ewu2lt3y"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hhsksonu.pythonanywhere.com']
 
 
 # Application definition
@@ -117,14 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
+# STATIC FILES (Production-safe)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS =[
-    BASE_DIR / "static"
-]
-
-MEDIA_ROOT = BASE_DIR / "uploads"
-MEDIA_URL = "/fiels/"
+# MEDIA FILES
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'uploads'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
